26f37959e9e7a2658cafc12081b6591ea5446ae9
Marcos Colella <colella.dev@gmail.com>
2024-02-05 13:44:59 -0300

    Cadastrar Notas

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -6,12 +6,21 @@ import { TextArea } from "../../components/TextArea"
 import { Button } from "../../components/Button"
 import { NoteItem } from "../../components/NoteItem"
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+import { api } from "../../services/api"
 
 
 export function New() {
 
-    const [ tags, setTags] = useState([])
-    const [ newTag, setNewTag] = useState("")
+    const [tags, setTags] = useState([])
+    const [newTag, setNewTag] = useState("")
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [rating, setRating] = useState("")
+
+    const navigate = useNavigate()
 
     function handleAddTag(){
         setTags( prevState => [...prevState, newTag] ) // Mantem oq tinha antes "prevState" e adiciona o newTag
@@ -21,6 +30,27 @@ export function New() {
       function handleDeleteTag(deleted) { //deleted recebe a Tag
         setTags(prevState => prevState.filter((tag, index) => index !== deleted))
     }
+
+    async function handleNewNote(){
+
+        if(!title){
+            return alert("Digite um título para sua nota")
+          }
+        
+          if(newTag){
+            return alert("Você deixou uma Tag no campo adicionar mas não clicou em adicionar.")
+          }
+
+        await api.post("/movieNotes", {
+            title,
+            description,
+            tags,
+            rating
+        })
+        
+        alert("Nota criada com sucesso!")
+        navigate("/")
+     }
 
     return (
         <Container>
@@ -34,11 +64,22 @@ export function New() {
             <h1>Novo filme</h1>
 
             <CustomTitle>
-            <Input placeholder="Título"/>
-            <Input placeholder="Sua nota (de 0 a 5)"/>
+            <Input 
+            placeholder="Título" 
+            onChange={e=> setTitle(e.target.value)}
+            />
+                                       
+            <Input 
+            placeholder="Sua nota (de 0 a 5)" 
+            onChange={e=> setRating(e.target.value)}
+            />
+
             </CustomTitle>
 
-            <TextArea placeholder="Observações"/>
+            <TextArea 
+            placeholder="Observações" 
+            onChange={e=> setDescription(e.target.value)}
+            />
 
             <h2>Marcadores</h2>
 
@@ -66,7 +107,7 @@ export function New() {
 
            <CustomButton>
             <Button title="Excluir filme"/>
-            <Button title="Salvar alterações"/>
+            <Button title="Salvar alterações" onClick = {handleNewNote}/>
             </CustomButton>
            
 
